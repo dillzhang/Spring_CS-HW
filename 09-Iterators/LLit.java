4ed0c72e59f46912203a8fc2bdd2f90baff2974a
Dillon Zhang <d1998z@gmail.com>
2015-03-22 21:43:55 -0400

    Finished

---
+++ 09-Iterators/LLit.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class LLit<E> implements Iterator<E>{
+    private Node<E> t;
+    private Node<E> x = null;
+
+    public LLit(Node<E> n){
+	t=n;
+    }
+    
+    public boolean hasNext(){
+	return t.getNext()!=null;
+    }
+    
+    public E next(){
+	E retval = t.getNext().getData();
+	    x = t;
+	t=t.getNext();
+	return retval;
+    }
+    
+    public void remove() {
+	if (x != nulld) {
+	    x.setNext(t.getNext());
+	    x = null;
+	} else {
+	    throw new IllegalStateException();
+	}
+    }
+}
